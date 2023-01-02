4c200098d51867c56ce6c6eacb23e19046b3a42c
portage dashboard <ahmedjamil@gitlab.com>
2023-01-02 11:10:21 +0500

    Updated auth to work for bearer tokens

--- src/middleware/apiAuth.js
+++ src/middleware/apiAuth.js
@@ -4,10 +4,10 @@ import { User } from '../models';
 const jwt = require('jsonwebtoken');
 
 const apiAuth = async (req, res, next) => {
-  if (!(req.headers && req.headers['x-token'])) {
+  if (!(req.headers && req.header('Authorization'))) {
     return errorResponse(req, res, 'Token is not provided', 401);
   }
-  const token = req.headers['x-token'];
+  const token = req.header('Authorization').replace('Bearer ', '')
   try {
     const decoded = jwt.verify(token, process.env.SECRET);
     req.user = decoded.user;
@@ -22,6 +22,7 @@ const apiAuth = async (req, res, next) => {
     req.user = reqUser;
     return next();
   } catch (error) {
+    // console.log("ShowMeHere",error)
     return errorResponse(
       req,
       res,
